f7d943a30fc08badae254fef701ed13dd6d2a298
nitesh <neeteshsah550@gmail.com>
2022-09-11 19:02:31 +0530

    initial commit

---
+++ partials/_nav.php
@@ -0,0 +1,39 @@
+<?php
+if(isset($_SESSION['loggedin']) && $_SESSION['loggedin']==true){
+    $loggedin= true;
+}  
+else{
+  $loggedin = false;
+}  
+echo '<nav class="navbar navbar-expand-lg bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="/logingsystem">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/logingsystem/welcome.php">Home</a>
+        </li>';
+        if(!$loggedin){
+        echo'<li class="nav-item">
+          <a class="nav-link" href="/logingsystem/login.php">Login</a>
+        <li class="nav-item">
+          <a class="nav-link " href="/logingsystem/signup.php" >Signup</a>
+        </li>';
+        }
+        if($loggedin){
+        echo '<li class="nav-item">
+          <a class="nav-link " href="/logingsystem/logout.php" >Logout</a>
+        </li>';
+        }
+      echo '</ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>';
+?>
---
+++ welcome.php
@@ -0,0 +1,30 @@
+<?php
+session_start();
+if(!isset($_SESSION['loggedin']) || $_SESSION['loggedin']!=true){
+  header("location: login.php");
+  exit;
+}
+
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Welcome - <?php $_SESSION['username'] ?> </title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+  </head>
+  <body>
+    <?php require 'partials/_nav.php' ?>
+    Welcome - <?php echo $_SESSION['username'] ?>
+    <div class="container my-4">
+      <div class="alert alert-success" role="alert">
+        <h4 class="alert-heading"> Welcome - <?php echo $_SESSION['username'] ?></h4>
+        <p>hey, how are u doing? Welcome , you are login as <?php echo $_SESSION['username'] ?>. Aww yeah, you successfully read this important alert message. This example text is going to run a bit longer so that you can see how spacing within an alert works with this kind of content.</p>
+        <hr>
+        <p class="mb-0">Whenever you need to, be sure to logout <a href="/logingsystem/logout.php">Using this link</a> </p>
+      </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+  </body>
+</html>
